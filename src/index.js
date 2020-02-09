1231c8a7842a5ea2e8b72342ac371491052c9a32
Randy <randyisbusy@gmail.com>
2020-02-08 17:06:30 -0700

    test to view changes between commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  console.log("DOM loaded per DOMContentLoaded event") 
+}); 
+
+
+let stack = document.getElementById("list")
+let stackList = document.createElement("div")
+stack.appendChild(stackList)
+stackList.setAttribute("class", "popOff")  
+let uL = document.createElement("ul")
+uL.setAttribute("class", "popOffList")
+stackList.appendChild(uL)  
+
+
+document.addEventListener("submit", (e) => {  
+  e.preventDefault(); 
+  let post = document.getElementById("tasks")  
+  let textToAdd = document.getElementById("new-task-description").value
+  let listItem = document.createElement('li') 
+  let button = document.createElement('button') 
+  button.setAttribute("class", "to-delete") 
+  button.innerText = textToAdd
+  listItem.innerHTML = textToAdd 
+  post.appendChild(listItem)   
+  makeListForDelete() 
+
+  function makeListForDelete(){ 
+  let addHere = document.getElementsByClassName("popOffList")
+  let lI = document.createElement('li') 
+  lI.appendChild(button)
+  uL.appendChild(lI) 
+  
+}
+  
+let destroyButton = document.getElementsByClassName("to-delete")
+destroyButton.addEventListener("click", () => { 
+console.log("HMMMMM")
+})
+
+} ) 
+
+
+
