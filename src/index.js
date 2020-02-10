e8f4cd1ba23bcf50eaa5c245ce258d804524da08
Randy <randyisbusy@gmail.com>
2020-02-10 01:52:01 -0700

    BDD; file works as expected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded per DOMContentLoaded event") 
-}); 
+  console.log("DOM loaded per DOMContentLoaded event") });
 
 
 let stack = document.getElementById("list")
@@ -11,31 +10,42 @@ let uL = document.createElement("ul")
 uL.setAttribute("class", "popOffList")
 stackList.appendChild(uL)  
 
-
 document.addEventListener("submit", (e) => {  
   e.preventDefault(); 
   let post = document.getElementById("tasks")  
   let textToAdd = document.getElementById("new-task-description").value
   let listItem = document.createElement('li') 
   let button = document.createElement('button') 
-  button.setAttribute("class", "to-delete") 
-  button.innerText = textToAdd
+  button.setAttribute("class", textToAdd) 
+  button.innerText = textToAdd 
+  listItem.setAttribute("class", textToAdd)
   listItem.innerHTML = textToAdd 
-  post.appendChild(listItem)   
-  makeListForDelete() 
+  post.appendChild(listItem)  
+  nestedNode = document.getElementsByClassName(textToAdd)
+  let arr = [...nestedNode]
+  arr[0].appendChild(button)
+     
+debugger
+ 
 
-  function makeListForDelete(){ 
-  let addHere = document.getElementsByClassName("popOffList")
-  let lI = document.createElement('li') 
-  lI.appendChild(button)
-  uL.appendChild(lI) 
-  
+button.onclick = (e) => { 
+  console.log("thats better", e) 
+  post.remove()
+  console.log("BOOMSHAKALAKA")
 }
-  
-let destroyButton = document.getElementsByClassName("to-delete")
-destroyButton.addEventListener("click", () => { 
-console.log("HMMMMM")
-})
+
+function doIt(event) {   
+  event.preventDefault
+  console.log("my firing event", event)
+  console.log("i was clicked")
+ let collection = document.querySelectorAll(".to-delete")  
+  for (let i = 0; i <  collection.length; i++){
+          collection[i].textContent === "Work out"
+  }
+ debugger
+ console.log(collection, collection[0]) 
+ 
+}
 
 } ) 
 
